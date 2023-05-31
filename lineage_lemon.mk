f0df7f217b64ffb40eccca39f4b7b7f52a1af049
Andy Vandijck <andyvand@gmail.com>
2023-05-31 12:01:37 +0200

    Xiaomi lemon upgrades

---
+++ lineage_lemon.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, device/xiaomi/lemon/device.mk)
+
+# Enable GAPPS built in.
+WITH_GAPPS := true
+
+# Inherit device configuration.
+$(call inherit-product, vendor/dot/config/common.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+PRODUCT_NAME := lineage_lemon
+PRODUCT_DEVICE := lemon
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=lemon \
+    BUILD_FINGERPRINT := "Redmi/lemon_global/lemon:13/SKQ1.211202.001/V15.0.3.0.SJQMIXM:user/release-keys"
+
