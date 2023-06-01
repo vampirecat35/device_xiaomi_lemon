0204b151f24b6394655c5bae4120939fa9cd421f
Andy Vandijck <andyvand@gmail.com>
2023-06-01 17:43:26 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_juice.mk \
     $(LOCAL_DIR)/lineage_lemon.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_lemon-user \
-    dot_lemon-userdebug \
-    dot_lemon-eng \
     lineage_lemon-user \
     lineage_lemon-userdebug \
     lineage_lemon-eng
