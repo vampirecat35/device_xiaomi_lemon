50d9555bbb3d5fdae923c6b796822f0c2aa11da1
Andy Vandijck <andyvand@gmail.com>
2023-06-18 21:59:12 +0200

    Fixes

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/xiaomi/lemon/juice-vendor.mk)
 
+# Inherit gapps
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Soong Namespace
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
