99a89e25f5aa3b5237ece89e63c7c4f1d309b881
Andy Vandijck <andyvand@gmail.com>
2023-06-19 00:26:32 +0200

    Add screen width and height

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_SCREEN_WIDTH = 1080
+TARGET_SCREEN_HEIGHT = 2340
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
